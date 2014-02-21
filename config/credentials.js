dfd46c9c2175122d1450898e6fbd75ee57251a81
Dan McKay <mckayefc@gmail.com>
2014-02-21 11:44:43 +0000

    rm

--- config/credentials.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  domain:       'fh-dan.auth0.com',
-  clientID:     'RngC3hcOfp7nUqea9yF3IvNZimkJt3Z4',
-  clientSecret: 'hJJan87MjOKw068qHLTSWawL4iZLioAq3Ua-En_fdgMlnCX0JKoEo_Q4y12HU2fm'
-};
